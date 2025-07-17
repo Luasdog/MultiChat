359e827be819581f1dee6017294ec6543b5e589d
Luasdog <cancersot@gmail.com>
2025-07-17 23:10:21 +0800

    feature: 实现grpc客户端发送请求的逻辑

---
+++ GateServer/VerifyGrpcClient.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <grpcpp/grpcpp.h>
+#include "message.grpc.pb.h"
+#include "const.h"
+#include "Singleton.h"
+
+using grpc::Channel;
+using grpc::Status;
+using grpc::ClientContext;
+
+using message::GetVerifyReq;
+using message::GetVerifyRsp;
+using message::VerifyService;
+
+class VerifyGrpcClient : public Singleton<VerifyGrpcClient>
+{
+	friend class Singleton<VerifyGrpcClient>;
+public:
+	GetVerifyRsp GetVerifyCode(std::string email) {
+		ClientContext context;
+		GetVerifyRsp reply;
+		GetVerifyReq request;
+		request.set_email(email);
+
+		Status status = _stub->GetVerifyCode(&context, request, &reply);
+		if (status.ok()) {
+			return reply;
+		}
+		else {
+			reply.set_error(ErrorCodes::RPCFailed);
+			return reply;
+		}
+	}
+
+private:
+	// 显示声明构造函数
+	VerifyGrpcClient() {
+		std::shared_ptr<Channel> channel = grpc::CreateChannel("0.0.0.0:50051",
+			grpc::InsecureChannelCredentials());
+		_stub = VerifyService::NewStub(channel);
+
+	}
+	std::unique_ptr<VerifyService::Stub> _stub;
+};
+
