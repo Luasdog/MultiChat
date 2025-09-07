c0e53de96f6be137b860f10e900e300e6e48fe50
Luasdog <cancersot@gmail.com>
2025-09-07 16:18:04 +0800

    feature: 服务端连接池

--- ChatServer/Singleton.h
+++ ChatServer/Singleton.h
@@ -6,11 +6,11 @@
 template <typename T>
 class Singleton {
 protected:
-    Singleton() = default;
-    Singleton(const Singleton<T>&) = delete;
-    Singleton& operator=(const Singleton<T>& st) = delete;
+	Singleton() = default;
+	Singleton(const Singleton<T>&) = delete;
+	Singleton& operator=(const Singleton<T>& st) = delete;
 
-    static std::shared_ptr<T> _instance;
+	static std::shared_ptr<T> _instance;
 public:
     static std::shared_ptr<T> GetInstance() {
         static std::once_flag s_flag;
