348abf912c1dcc2fc197c70ba4be3818606c3fb0
Luasdog <cancersot@gmail.com>
2025-09-07 22:37:05 +0800

    feature: 服务器查询、添加好友

--- ChatServer/MysqlMgr.h
+++ ChatServer/MysqlMgr.h
@@ -11,7 +11,11 @@ public:
 	bool CheckEmail(const std::string& name, const std::string& email);
 	bool UpdatePwd(const std::string& name, const std::string& email);
 	bool CheckPwd(const std::string& email, const std::string& pwd, UserInfo& userInfo);
+	bool AddFriendApply(const int& from, const int& to);
+	bool AuthFriendApply(const int& from, const int& to);
+	bool AddFriend(const int& from, const int& to, std::string back_name);
 	std::shared_ptr<UserInfo> GetUser(int uid);
+	std::shared_ptr<UserInfo> GetUser(std::string name);
 
 private:
 	MysqlMgr();
